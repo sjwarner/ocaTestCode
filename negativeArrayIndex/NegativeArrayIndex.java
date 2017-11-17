bc0d2dc21d0d0949ef1a976fe9ee1c08f9cf08e0
Sam Warner <sam.warner@blackpepper.co.uk>
2017-11-17 08:37:21 +0000

    Add negativeArrayIndex to exercises

---
+++ negativeArrayIndex/NegativeArrayIndex.java
@@ -0,0 +1,16 @@
+// This small code snippet is to demonstrate that Java
+// compiler doesn't check the range of the index positions
+// at which you try to access an array element.
+
+// The following will still compile, even though it references
+// a negative element in the array. It will, however, throw an
+// ArrayIndexOutOfBounds exception at runtime.
+
+public class NegativeArrayIndex {
+
+	public static void main(String args[]) {
+		int intArray[] = new int[2];
+		System.out.println(intArray[-10]);
+	}
+
+}
