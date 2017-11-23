fe2d1bef7c01e49c0cb929aae56f44f3398b06c6
Sam Warner <sam.warner@blackpepper.co.uk>
2017-11-23 11:11:55 +0000

    Update commments in StaticImportPi.java

--- staticImportPi/StaticImportPi.java
+++ staticImportPi/StaticImportPi.java
@@ -1,3 +1,6 @@
+// The following aims to illustrate that static fields need to be imported using the
+// import static syntax. Failure to do so will cause compile time errors.
+
 import static java.lang.Math.PI; // Static values (like PI) need to be statically imported
 // import java.lang.Math.PI; <--- Importing it non-statically will cause compile errors
 
