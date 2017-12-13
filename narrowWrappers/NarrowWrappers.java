1e3fc1f9a85ca4c22f3c08dd03e5d5525930dec5
Sam Warner <sam.warner@blackpepper.co.uk>
2017-12-13 12:11:28 +0000

    add narrowWrappers

---
+++ narrowWrappers/NarrowWrappers.java
@@ -0,0 +1,16 @@
+class NarrowWrappers {
+
+    public static void main(String args[]) {
+
+        Double D = 1.0d;
+
+        square(D);
+
+    }
+
+    private void square(Float F) {
+
+        System.out.println(F * F);
+
+    }
+}
