48433d7916d26841e1dfe0028b83f8a5f62d8140
Sam Warner <sam.warner@blackpepper.co.uk>
2017-12-13 12:12:01 +0000

    add validIdentifiers

---
+++ validIdentifiers/ValidIdentifiers.java
@@ -0,0 +1,13 @@
+public class ValidIdentifiers {
+
+    public static void main(String args[]) {
+
+        int _ = 0;
+        int 2a = 0;
+        int first name = 0;
+        int new = 0;
+        int default = 0;
+
+    }
+
+}
