bba7d1f5cb23c73d483828a7fc7ed3aa590054a8
Sam Warner <sam.warner@blackpepper.co.uk>
2017-12-13 12:12:10 +0000

    add widenWrappers

---
+++ widenWrappers/WidenWrappers.java
@@ -0,0 +1,16 @@
+class WidenWrappers {
+
+    public static void main(String args[]) {
+
+        Float F = 1.0f;
+
+        square(F);
+
+    }
+
+    private void square(Double D) {
+
+        System.out.println(D * D);
+
+    }
+}
